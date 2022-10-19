f92be297a7f233bf90b4b3d5376c8e3cc297c771
K4346 <laeyenix@gmail.com>
2022-10-20 01:42:27 +0300

    v0.8

---
+++ src/CustomChart.js
@@ -0,0 +1,106 @@
+class CustomChart {
+
+    constructor(canvas) {
+        this.canvas = canvas
+    }
+
+    clear() {
+        this.canvas.getContext(`2d`).clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+
+    arrow(values) {
+        function floatFixed(fValue){
+            return parseFloat( fValue.toFixed(8) );
+        }
+        this.clear()
+        let text
+        let fromx = 5, fromy = 0, tox = 75, toy
+        if (values[0].value === values[1].value) {
+            toy = 0
+            text = 'Не изменилось'
+        } else if (values[0].value < values[1].value) {
+            fromy = 70
+            toy = 0
+            text= `+${floatFixed(values[1].value-values[0].value)}₽`
+        } else {
+            fromy = 0
+            toy = 70
+            text= `${floatFixed(values[1].value-values[0].value)}₽`
+        }
+        const ctx = this.canvas.getContext(`2d`)
+        ctx.beginPath();
+        canvas_arrow(ctx, fromx, fromy, tox, toy);
+        ctx.font = `18px Tahoma`;
+        ctx.fillText(text, tox + 5, (fromy + toy) / 2);
+        // canvas_arrow(ctx, 100, 200, 400, 50);
+        // canvas_arrow(ctx, 200, 30, 10, 150);
+        // canvas_arrow(ctx, 400, 200, 100, 50);
+        ctx.stroke();
+
+
+        function canvas_arrow(context, fromx, fromy, tox, toy) {
+            var headlen = 25;
+            var dx = tox - fromx;
+            var dy = toy - fromy;
+            var angle = Math.atan2(dy, dx);
+            context.moveTo(fromx, fromy);
+            context.lineTo(tox, toy);
+            context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+            context.moveTo(tox, toy);
+            context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+        }
+    }
+
+    draw(values) {
+        const max = Math.max(...values.map(o => o.value))
+        const ctx = this.canvas.getContext(`2d`)
+
+
+        const Gap = {
+            HORIZONTAL: 100,
+            VERTICAL: 30
+        }
+
+        const BarCoordinate = {
+            INITIAL_X: 80,
+            INITIAL_Y: 0
+        }
+
+        const BarSize = {
+            MAX_HEIGHT: this.canvas.height,
+            WIDTH: 50
+        };
+
+        const LabelCoordinate = {
+            INITIAL_X: 30,
+            INITIAL_Y: 70
+        }
+
+
+        this.clear()
+
+        let currentBarX = BarCoordinate.INITIAL_X;
+        let currentLabelY = LabelCoordinate.INITIAL_Y;
+
+        const gapBetweenBars = BarSize.WIDTH + Gap.HORIZONTAL;
+
+        for (let i = 0; i < values.length; i++) {
+
+            const barHeight = (values[i].value * BarSize.MAX_HEIGHT) / max;
+            ctx.save();
+
+            ctx.translate(0, this.canvas.height);
+            ctx.rotate(-Math.PI / 2);
+            ctx.fillText(values[i].date.toUpperCase(), LabelCoordinate.INITIAL_X, currentLabelY);
+            ctx.restore();
+
+
+            ctx.fillRect(currentBarX, BarCoordinate.INITIAL_Y, BarSize.WIDTH, barHeight);
+
+            currentBarX += gapBetweenBars;
+            currentLabelY += gapBetweenBars;
+        }
+    }
+}
+
+export {CustomChart}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,92 @@
-import {MiniMaple} from "./miniMaple.js";
+import {CurrencyEntity} from "./CurrencyEntity.js";
+import {GeneralEntity} from "./GeneralEntity.js";
+import {CustomChart} from "./CustomChart.js";
 
 document.addEventListener('DOMContentLoaded', setup)
 
+
+const currencySelector = document.getElementById('currencySelector');
+const nominalInput = document.getElementById('nominalInput');
+const chart = new CustomChart(document.getElementById('canvas'))
+
+const API = 'https://www.cbr-xml-daily.ru/daily_json.js'
+let info
+let currency
+
+// TODO СДЕЛАТЬ ПРОГРЕССБАР
 function setup() {
-    document.getElementById('submitButton').onclick = something;
+    startApp()
 }
 
-function something() {
-    const api = 'https://asli-fun-fact-api.herokuapp.com/'
 
-    const someDummyDiv = document.createElement('div');
-    someDummyDiv.classList.add('generated');
-    let url = 'https://example.com';
+function calculateResult() {
+    const nominal = Number(nominalInput.value)
+    if (currencySelector.value === '-' || !Number.isInteger(nominal) || nominal < 1) {
+        document.getElementById('output').textContent = '\n';
+        return
+    }
+    const currencyValue = parseFloat(currency.value) * nominal / parseFloat(currency.nominal)
+    const result = document.getElementById('output');
+    result.textContent = `${nominal.toString()} ${currency.charCode} = ${currencyValue.toString()}₽`
+}
 
-    fetch(api)
+function makeChart() {
+    chart.arrow([
+        {
+            value: currency.previous,
+            date: info.previousDate
+        }, {
+            value: currency.value,
+            date: info.date
+        }])
+}
+
+function initCurrencySelector(currencies) {
+    currencies.forEach(i => {
+        let opt = document.createElement('option');
+        opt.value = i
+        opt.innerHTML = i
+        currencySelector.appendChild(opt)
+    })
+    currencySelector.addEventListener('change', (event) => {
+        const selectedName = document.getElementById('selectedCurrencyContainer')
+        if (event.target.value !== '-') {
+            currency = new CurrencyEntity(info.valute[currencySelector.value])
+            selectedName.textContent = `Валюта: ${currency.name}`
+            document.getElementById('changesContainer').style.visibility='visible'
+            makeChart()
+        } else {
+            document.getElementById('changesContainer').style.visibility='hidden'
+            selectedName.textContent = `\n`
+            chart.clear()
+        }
+        calculateResult()
+    });
+}
+
+function initNominalInput() {
+    nominalInput.addEventListener('input', () => {
+        calculateResult()
+    });
+
+}
+
+function initViews() {
+    initCurrencySelector(Object.keys(info.valute))
+    initNominalInput()
+}
+
+function startApp() {
+    fetch(API)
         .then(res => res.json())
         .then((out) => {
-            console.log('Checkout this JSON! ', out);
-        })
-        .catch(err => {
-            throw err
-        });
-    const container = document.getElementById('container');
-    // while (container.firstChild) {
-    //     container.removeChild(container.firstChild);
-    // }
-    container.appendChild(someDummyDiv);
+            if (info != null) return
+            info = new GeneralEntity(out)
+            initViews()
+        }).catch(err => {
+        setTimeout(function() {
+            startApp()
+        }, 2000);
+        console.error(err)
+    });
 }
